9758f5c0d9ae4373cd4032581bd91bdaf7be9466
Zakhar <zaharkhachaturyan@gmail.com>
2025-04-23 19:37:54 +0200

    Changed ThemeToggler, added icons.

--- src/shared/ui/Theme/ui/ThemeToggler.tsx
+++ src/shared/ui/Theme/ui/ThemeToggler.tsx
@@ -1,11 +1,19 @@
+import { DarkModeIcon } from "@shared/assets/Components/DarkModeIcon";
 import { useThemeStore } from "../model/useThemeStore";
-//TODO: немного переделать дизайн тоглера 
+
+import { LightModeIcon } from "@shared/assets/Components/LightModeIcon";
+
+//TODO: немного переделать дизайн тоглера
 export const ThemeToggler = () => {
-    const { theme, toggleTheme } = useThemeStore();
+  const { theme, toggleTheme } = useThemeStore();
 
   return (
-    <button onClick={toggleTheme} className="bg-accent text-white px-4 py-2 rounded">
-      Switch to {theme === 'light' ? 'dark' : 'light'} mode
+    <button onClick={toggleTheme} className="rounded-[100%] p-[8px]  text-white max-w-[40px]">
+      {theme === "light" ? (
+          <DarkModeIcon/>
+      ) : (
+          <LightModeIcon />
+      )}
     </button>
   );
-}
+};
